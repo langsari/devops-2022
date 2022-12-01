d8c4247434a4c0f05eec0771d60cfdc32f8a54be
RealAzzam <just.azzamm@gmail.com>
2022-12-01 16:22:25 +0700

    line 8

--- kholed-langsari.py
+++ kholed-langsari.py
@@ -4,10 +4,8 @@ print("keep it up")
 print ("hello")
 print ("Hello error404")
 print("Hello from Khaled C:")
-
 print("checkmate")
-
-
+print("development and opration")
 print ("hello")
 print ("hello")
 print ("Hello :)")

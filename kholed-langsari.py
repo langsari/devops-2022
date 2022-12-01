cd2be71ede4918ec139d22ce43d91c35ea9b6366
RanaLordeng <96864385+Rana-Loerdeng@users.noreply.github.com>
2022-12-01 16:24:14 +0700

    Rana

--- kholed-langsari.py
+++ kholed-langsari.py
@@ -4,8 +4,10 @@ print("keep it up")
 print ("hello")
 print ("Hello error404")
 print("Hello from Khaled C:")
+
 print("checkmate")
-print("development and opration")
+
+print("hi")
 print ("hello")
 print ("hello")
 print ("Hello :)")

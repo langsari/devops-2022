390766b553792211c1cffc269311fa7069bfb2bb
RealAzzam <just.azzamm@gmail.com>
2022-12-01 16:25:58 +0700

    any

--- kholed-langsari.py
+++ kholed-langsari.py
@@ -1,11 +1,9 @@
 print ("Hello")
 print("keep it up")
-
 print ("hello")
 print ("Hello error404")
 print("Hello from Khaled C:")
 print("checkmate")
-print("development and opration")
 print ("hello")
 print ("hello")
 print ("Hello :)")
